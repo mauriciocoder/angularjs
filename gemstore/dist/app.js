7bdf6eb0a5c414bb6ff843e497b5520414abec47
mauriciocoder <mauricio.coder@outlook.com>
2017-06-30 14:55:13 -0300

    built-in directives

--- gemstore/dist/app.js
+++ gemstore/dist/app.js
@@ -1,6 +1,20 @@
 (function() {
   var app = angular.module('store', [])
+  var gems = [
+    {
+      name: 'Gem1',
+      price: 12.50,
+      description: 'Gem1 description',
+      canSell: true
+    },
+    {
+      name: 'Gem2',
+      price: 120.50,
+      description: 'Gem2 description',
+      canSell: false
+    },
+  ]
   app.controller('StoreController', function() {
-    this.product = {name: 'Product 1', price: 13.50}
+    this.products = gems
   })
 })()

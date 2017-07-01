e9901775ea1a92a56d91b687f420b7e9bc6bb27d
mauriciocoder <mauricio.coder@outlook.com>
2017-07-01 11:44:59 -0300

    tabs - lecture 2.6

--- gemstore/dist/app.js
+++ gemstore/dist/app.js
@@ -5,16 +5,33 @@
       name: 'Gem1',
       price: 12.50,
       description: 'Gem1 description',
-      canSell: true
+      specs: 'Spec Gem1',
+      reviews: 'Review Gem1',
+      canSell: true,
+      images: ['http://pre05.deviantart.net/e7cc/th/pre/f/2012/199/0/8/gem_png_by_doloresdevelde-d57oyqr.png',
+      'https://www.ajsgem.com/sites/default/files/pink-sapphire-gem-madagascar-1.25cts.jpg']
     },
     {
       name: 'Gem2',
       price: 120.50,
       description: 'Gem2 description',
-      canSell: true
+      specs: 'Spec Gem2',
+      reviews: 'Review Gem2',
+      canSell: true,
+      images: ['http://www.roadtripsavings.com/images/blue-gem.jpg',
+      'http://planbee.biz/wp-content/uploads/2012/07/Blue-Diamond.png']
     },
   ]
   app.controller('StoreController', function() {
     this.products = gems
+  })
+  app.controller('TabController', function() {
+    this.content = 0
+    this.setContent = function(c) {
+      this.content = c
+    }
+    this.isContent = function(c) {
+      return this.content === c
+    }
   })
 })()

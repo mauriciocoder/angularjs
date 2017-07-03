a2d8ada4c317a21538d1ead7e9bd46a288772c6d
mauriciocoder <mauricio.coder@outlook.com>
2017-07-03 10:28:59 -0300

    forms - lecture 3.4

--- gemstore/dist/app.js
+++ gemstore/dist/app.js
@@ -6,7 +6,18 @@
       price: 12.50,
       description: 'Gem1 description',
       specs: 'Spec Gem1',
-      reviews: 'Review Gem1',
+      reviews: [
+        {
+          stars: 2,
+          body: 'That gem is awesome',
+          author: 'mauricio.coder@outlook.com'
+        },
+        {
+          stars: 4,
+          body: 'That gem rocks!!!!',
+          author: 'bonetti.mauricio@outlook.com'
+        }
+      ],
       canSell: true,
       images: ['http://pre05.deviantart.net/e7cc/th/pre/f/2012/199/0/8/gem_png_by_doloresdevelde-d57oyqr.png',
       'https://www.ajsgem.com/sites/default/files/pink-sapphire-gem-madagascar-1.25cts.jpg']
@@ -16,7 +27,7 @@
       price: 120.50,
       description: 'Gem2 description',
       specs: 'Spec Gem2',
-      reviews: 'Review Gem2',
+      reviews: [],
       canSell: true,
       images: ['http://www.roadtripsavings.com/images/blue-gem.jpg',
       'http://planbee.biz/wp-content/uploads/2012/07/Blue-Diamond.png']
